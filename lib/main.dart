fd1bf0ffd0b241393bee8aa76707947c2945d15e
MD Hafizur Rahman <minhajuddin50013@gmail.com>
2025-11-24 00:41:00 +0600

    shoffy

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:ecommerce_app/app/route/app_route.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(MyApp());
 }
 
