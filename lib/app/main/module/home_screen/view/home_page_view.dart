ee3d3eb0eeaaee9c95fbc7dc7ee4dbfc01b4d887
MD Hafizur Rahman <minhajuddin50013@gmail.com>
2025-11-26 00:53:36 +0600

    shoffy

--- lib/app/main/module/home_screen/view/home_page_view.dart
+++ lib/app/main/module/home_screen/view/home_page_view.dart
@@ -1,10 +1,30 @@
+import 'package:ecommerce_app/app/main/module/auth_screen/bloc/auth_bloc.dart';
+import 'package:ecommerce_app/app/main/module/auth_screen/bloc/auth_event.dart';
+import 'package:ecommerce_app/app/main/module/auth_screen/bloc/auth_state.dart';
+import 'package:ecommerce_app/app/main/module/home_screen/bloc/home_bloc.dart';
+import 'package:ecommerce_app/app/main/module/home_screen/bloc/home_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Home Page"),
+      automaticallyImplyLeading: false,
+      ),
+      body: BlocConsumer<HomeBloc, HomeState>(
+          listener: (context, state){
+          },
+          builder: (context, state) {
+            return SafeArea(child: Column(children: [
+              ElevatedButton(onPressed: (){
+                context.read<AuthBloc>().add(AuthLogOut() as AuthEvent);
+              }, child: Text("LogOut"))
+            ],));
+          }, ),
+    );
   }
 }
